fed12e2cdb30fadbb7f0c8ad901d766856eaff5c
lexandr0s <lexandros71@gmail.com>
2018-10-31 15:52:11 +0400

    install script

--- as-setup.sh
+++ as-setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+apt update
+apt install -y bc
 cd /tmp
-pwd
-ls
+wget https://github.com/lexandr0s/autoswitch/raw/master/autoswitch.tar
+tar -xvf autoswitch.tar
+cp as/autoswitch /hive/sbin
+cp as/autoswitch_bin /hive/sbin
+cp as/autoswitch.conf /hive-config
+cp as/rig_data.json /hive-config
+rm -R as
+rm autoswitch.tar
+cd /home/user
+
