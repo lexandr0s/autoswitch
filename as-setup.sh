208103eab645bfd4fa85a255ef8af23b60d312ad
lexandr0s <lexandr0s@users.noreply.github.com>
2018-11-04 21:21:54 +0400

    Update as-setup.sh

--- as-setup.sh
+++ as-setup.sh
@@ -9,18 +9,6 @@ cp as/autoswitch /hive/sbin
 cp as/autoswitch_bin /hive/sbin
 if [[ ! -f /hive-config/autoswitch.conf ]]; then
 	cp as/autoswitch.conf /hive-config
-else
-	sed -i '/PROFILE/d' /hive-config/autoswitch.conf
-	sed -i '/different miners/d' /hive-config/autoswitch.conf
-	sed -i '/FS with names like this/d' /hive-config/autoswitch.conf
-	sed -i '1i\\n' /hive-config/autoswitch.conf
-	sed -i '1i\PROFILE=\"\"' /hive-config/autoswitch.conf
-	sed -i '1i\#If you are not using different profiles, just leave PROFILE variable empty.' /hive-config/autoswitch.conf
-	sed -i '1i\#\"Autoswitch Algorithm PROFILE\" and autoswitch on this riga will use FS only with this PROFILE in the name' /hive-config/autoswitch.conf
-	sed -i '1i\#If this variable is set, you can create a FS with names like this:' /hive-config/autoswitch.conf
-	sed -i '1i\#For example, with different miners (for NVIDIA and AMD rigs) or with different wallet addresses' /hive-config/autoswitch.conf
-	sed -i '1i\#Set the PROFILE variable if you want to create several different FS for one algorithm' /hive-config/autoswitch.conf
-	sed -i '/^$/N;/\n$/N;//D' /hive-config/autoswitch.conf
 fi
 
 
