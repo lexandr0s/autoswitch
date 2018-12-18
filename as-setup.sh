a78a1f66f30cdad2ac7ca4d8096eb993b4c87537
lexandr0s <lexandros71@gmail.com>
2018-12-18 18:04:08 +0400

    1.7 : Ass energy costs in the calculations

--- as-setup.sh
+++ as-setup.sh
@@ -5,28 +5,26 @@ if [[ $? -ne 0 ]]; then
 	apt update
 	apt install -y bc
 fi
+
 cd /tmp
 wget https://github.com/lexandr0s/autoswitch/raw/master/autoswitch.tar
 tar -xf autoswitch.tar 
 cp as/autoswitch /hive/sbin
 cp as/autoswitch_bin /hive/sbin
+
 if [[ ! -f /hive-config/autoswitch.conf ]]; then
 	cp as/autoswitch.conf /hive-config
-else
-	source /hive-config/autoswitch.conf
-	[[ $(echo $BENCHMARK | jq .Zhash) == null ]] &&	sed -i "s/}'/,\n\"Zhash\":0\n}'/" /hive-config/autoswitch.conf
 fi
 
 
 if [[ ! -f /hive-config/rig_data.json ]]; then
 	cp as/rig_data.json /hive-config
-else
-	rig_data=$(cat /hive-config/rig_data.json)
-	if [[ -z $(echo $rig_data | jq ".[] | select (.nice_algo == 36) | .algo") ]]; then
-		rig_data=$(echo $rig_data | jq ".[. | length] |= . + {\"hive_fs\": \"Autoswitch Zhash\",\"algo\": \"Zhash\",\"bench\": 0,\"mining\": 1,\"mult\": 0,\"nice_algo\": 36,\"fs_id\": 0}")
-		echo $rig_data | jq . > /hive-config/rig_data.json
-	fi
 fi
+
+if [[ ! -f /hive-config/autoswitch_pow.conf ]]; then
+	cp as/autoswitch_pow.conf /hive-config
+fi
+
 rm -R as
 rm autoswitch.tar
 cd /home/user
